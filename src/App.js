fc402eed905f97b0acbe4bc055819e0251ee48cd
Nicklaus McClendon <nicklaus@kulinacs.com>
2019-08-26 15:47:45 -0500

    Add basic counter functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar';
+import Counter from './components/Counter';
 
 const App = () => {
   return (
     <div className="App">
       <NavBar />
-      App Content Here
+      <Counter />
     </div>
   );
 }
