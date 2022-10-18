9bf87622a46b37032f661b0fcd5d391c9495ebe0
yaosam521 <yaosam.521@gmail.com>
2022-10-18 18:00:45 -0500

    New Feature

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,7 +4,7 @@ import Navbar from 'react-bootstrap/Navbar';
 const NavBar = () => {
   return (
     <Navbar bg="dark" variant="dark">
-      <Navbar.Brand href="#home">Counter</Navbar.Brand>
+      <Navbar.Brand href="#home">NEW BRAND NEW BRAND</Navbar.Brand>
     </Navbar>
   );
 }
